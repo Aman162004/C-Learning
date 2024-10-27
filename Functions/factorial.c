8fc86c31f56433d26efaa53ff2c66058e9342db2
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2024-10-27 22:01:37 +0530

    Add files via upload

---
+++ Functions/factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+int main()
+{
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("Factorial of %d is %d\n", x, factorial(x));
+    return 0;
+}
